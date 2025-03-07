43be8aca1b312fc808a2af5b0994153578359be6
Busra Yagcioglu <95376566+Busrwa@users.noreply.github.com>
2025-03-08 00:24:59 +0300

    Delete projectCreater directory

--- projectCreater/urls.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from django.urls import path, include
-
-urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('auth/', include('your_app_name.urls')),  # Auth işlemleri için uygulamanızın URLsini dahil edin
-    path('projeler/', include('projeler_app_name.urls')),  # Projeler sayfası için URL'yi dahil edin
-]
